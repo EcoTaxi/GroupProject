657b132ffccf4f489d25988c7c6bb30989bda8b1
PatrickGondouin <43571661+PatrickGondouin@users.noreply.github.com>
2018-12-30 22:09:04 +0000

    Patrick - Config file for Paypal Payments

---
+++ Config.java
@@ -0,0 +1,6 @@
+package com.example.joeco.ecotaxiphoneapp.config;
+
+public class Config {
+    public static final String PAYPAL_CLIENT_ID = "ASnKpmGhxvxmEQQkMgQzyMWSlYSL0nYsKfmVVVgl_CIboUqXNs5QR7T3KbBJ3gS7By8cvazCORPYoTd7";
+
+}
